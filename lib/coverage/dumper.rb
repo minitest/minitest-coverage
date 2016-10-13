154a46548dad864fba38c3d43530bae5da1f046f
Ryan Davis <ryand@zenspider.com>
2016-10-13 15:01:45 -0800

    Added coverage/start to pruner/dumper and renamed output to coverage.json [git-p4: depot-paths = "//src/minitest-coverage/dev/": change = 10912]

--- lib/coverage/dumper.rb
+++ lib/coverage/dumper.rb
@@ -1,10 +1,11 @@
 require "json"
+require "coverage/start"
 
 at_exit { # get behind minitest if we're running alongside
   at_exit {
     coverage = Coverage.result
 
-    File.open "baseline.json", "w" do |f|
+    File.open "coverage.json", "w" do |f|
       f.puts JSON.pretty_generate coverage
     end
   }
--- lib/coverage/pruner.rb
+++ lib/coverage/pruner.rb
@@ -1,4 +1,5 @@
 require "json"
+require "coverage/start"
 
 pwd_re = Regexp.union Dir.pwd
 pruner = lambda { |k,_| k !~ pwd_re }
@@ -7,7 +8,7 @@ at_exit { # get behind minitest if we're running alongside
   at_exit {
     coverage = Coverage.result.reject(&pruner)
 
-    File.open "baseline.json", "w" do |f|
+    File.open "coverage.json", "w" do |f|
       f.puts JSON.pretty_generate coverage
     end
   }
